3da0aa4d85318f0a78c8845a82108d3046ddbf34
dustxdustx <dustx@163.com>
2019-11-16 21:28:30 +0800

    加工view及模板

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
-from  django.template import loader
+from django.shortcuts import render,get_object_or_404
+
+#from  django.template import loader
 # Create your views here.
 def index(request):
 	lastest_question_list=Question.objects.order_by('-pub_date')[:5]
-	template=loader.get_template('polls/index.html')
+	template='polls/index.html'
+	#loader.get_template('polls/index.html')
 	context={
 	'lastest_question_list':lastest_question_list,
 	}
-	return HttpResponse(template.render(context,request))
+	return render(request,template,context)
+	#HttpResponse(template.render(context,request))
 
 def detail(request,question_id):
-	return HttpResponse("你正在看問題及選項 %s" %question_id)
+	question=get_object_or_404(Question,pk=question_id)
+	return render(request,'polls/detail.html',{'question':question})
+
 def results(request,question_id):
 	return HttpResponse("你真再看問題%s的投票結果"%question_id)
 def vote(request,question_id):
